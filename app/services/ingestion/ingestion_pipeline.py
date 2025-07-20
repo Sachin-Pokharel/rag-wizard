1d300b7665333cbaa9b98228f8ed790906fb32ff
Sachin-Pokharel <sachinwork33@gmail.com>
2025-07-20 14:56:23 +0545

    ingestion pipeline

--- app/services/ingestion/ingestion_pipeline.py
+++ app/services/ingestion/ingestion_pipeline.py
@@ -17,10 +17,19 @@ def ingest(
     docs = get_loader(loader_type)
     documents = docs.load(folder_path)
     typer.echo(f"✅ Done: {len(documents)} chunks processed.")
+    typer.echo(f"✅ Page Content: \n\n{documents[0].page_content}")
     chunker = get_chunker(chunker_type="recursive", chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     documents = chunker.chunk(documents)
+    typer.echo(f"✅ Page Content: \n\n{documents[0].page_content}")
+    typer.echo(f"✅ Metadata: \n\n{documents[0].metadata}")
     vector_store = MilvusVectorStore(collection_name="resume_docs", uri=settings.milvus_url, token=settings.milvus_token, dim=1536)
-    vector_store.embed_and_store_documents(documents[:1], embedder="openai")
+    results = vector_store.search_dense(
+        query="What is Sachin Pokharel?",
+        embedder="openai",
+        top_k=2
+    )
+    typer.echo(f"✅ Search Results: \n\n{results}")
+    #vector_store.embed_and_store_documents(documents, embedder="openai")
     typer.echo(f"✅ Done: {len(documents)} chunks after chunker processed.")
     # embed and store
 
